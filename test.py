b1ca683a50a21b77a5a83bd51bccf78eec467509
Nafisk312 <90558331+Nafisk312@users.noreply.github.com>
2023-07-16 22:27:35 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 from browser import document
-document <= "Hello world !"
+from browser.widgets.dialog import InfoDialog
+
+def click(ev):
+    InfoDialog("Hello", f"Hello, {document['btn6'].value} !")
+
+# bind event 'click' on button to function echo
+document["echo"].bind("click", click)
