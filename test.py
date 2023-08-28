1f5a48021edfb72cf9723e497027af526501572f
Nafisk312 <90558331+Nafisk312@users.noreply.github.com>
2023-08-28 14:09:09 +0300

    upd

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
 from browser import html, document
-from browser.widgets.dialog import InfoDialog
-
-def click(ev):
-    InfoDialog("Hello", f"Hello, {document['btn1'].value} !")
-
-# bind event 'click' on button to function echo
-document["echo"].bind("click", click)
 
 document['sell'] <= html.H4('4999')
