7849e4eaa94a6bd87a3cc790b1b2b2c62baebe9f
Nafisk312 <90558331+Nafisk312@users.noreply.github.com>
2023-08-28 14:57:40 +0300

    Update glister.py

--- item/glister.py
+++ item/glister.py
@@ -1,3 +1,8 @@
 from browser import html, document
 
+document['sell'] <= html.H4('4999')
+document['sell2'] <= html.H4('4999')
+document['sell3'] <= html.H4('4999')
 document['sell4'] <= html.H4('4999')
+document['sell5'] <= html.H4('4999')
+document['sell6'] <= html.H4('4999')
