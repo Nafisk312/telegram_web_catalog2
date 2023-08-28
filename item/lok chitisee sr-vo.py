a4668780503dc0dfc986fd4c5893e2b39066b7f4
Nafisk312 <90558331+Nafisk312@users.noreply.github.com>
2023-08-28 14:37:51 +0300

    Update lok chitisee sr-vo.py

--- item/lok chitisee sr-vo.py
+++ item/lok chitisee sr-vo.py
@@ -2,5 +2,6 @@ from browser import html, document
 import parsercurs
 
 curs = parsercurs.exchange("9afbe13e92d995dfc17befa52e6af754", "398", "643")
+price = 5999 // curs
 
-document['sell5'] <= html.H4(curs)
+document['sell5'] <= html.H4(price)
